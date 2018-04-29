7716b89f931a98c3b1dc1f1ffa8d5b95e8f70c7d
mikboy018 <boyer.mike.e@gmail.com>
2018-04-29 18:17:01 -0400

    Thread-based concurrency added

--- BookmarkServer.py
+++ BookmarkServer.py
@@ -45,6 +45,8 @@ import http.server
 import requests
 from urllib.parse import unquote, parse_qs
 import os
+import threading
+from socketserver import ThreadingMixIn
 
 memory = {}
 
@@ -66,7 +68,8 @@ form = '''<!DOCTYPE html>
 {}
 </pre>
 '''
-
+class ThreadHTTPServer(ThreadingMixIn, http.server.HTTPServer):
+        "This is an http server that supports thread-based concurrency"
 
 def CheckURI(uri, timeout=5):
     '''Check whether this URI is reachable, i.e. does it return a 200 OK?
@@ -156,5 +159,5 @@ class Shortener(http.server.BaseHTTPRequestHandler):
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8000))
     server_address = ('', port)
-    httpd = http.server.HTTPServer(server_address, Shortener)
+    httpd = http.server.ThreadHTTPServer(server_address, Shortener)
     httpd.serve_forever()
